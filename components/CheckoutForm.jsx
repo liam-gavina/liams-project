046ef296f4475d874f1d88cd84ce5108c5bc8768
liam-gavina <gemgavina22@gmail.com>
2023-12-05 22:23:42 -0700

    added buy now and  CheckoutForm

---
+++ components/CheckoutForm.jsx
@@ -0,0 +1,26 @@
+
+
+ 
+ function CheckoutForm ( {children, className}){
+   
+ 
+    const details={
+        id:"from dashboard",
+        quanity:2,
+        price:"from dashboard"
+    }
+
+  
+    return (
+        <form action='/api/checkout' method="POST" >
+            <input type="hidden" name="id" value="from dashboard"  />
+            <input type="hidden" name="price" value="price-10something" />
+            <button type="submit" id="checkout-button">Checkout</button>
+        </form>
+    )
+ }
+
+
+
+ export {CheckoutForm}
+ 
